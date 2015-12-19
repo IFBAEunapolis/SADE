c2921eb09c91692c557a33917808b5d621bfbc09
Leonardo Rufino <leonardo.nr0@gmail.com>
2015-12-19 16:12:30 -0300

    Criação da tela JSF PlanoAula.xhtml com integração com o PlanoAulaBean

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/controller/PlanoAulaBean.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/controller/PlanoAulaBean.java
@@ -1,5 +1,33 @@
 package br.edu.ifba.eunapolis.gestoacademica.controller;
 
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "plano")
 public class PlanoAulaBean {
+
+	private String descricao;
+	private String turma;
+	private String mensagem;
+
+	public void MensagemCadastro(){
+		this.mensagem = "Turma: "+this.turma+" Descrição: "+this.descricao+" Cadastrados com Sucesso!";
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getTurma() {
+		return turma;
+	}
+	public void setTurma(String turma) {
+		this.turma = turma;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+
+
 
 }
