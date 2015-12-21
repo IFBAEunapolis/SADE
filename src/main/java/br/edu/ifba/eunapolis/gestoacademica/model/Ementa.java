681e50a88a15f7d1a6afad1455b10bd1481db97b
Cliente <Cliente@CLIENTE>
2015-12-21 10:58:35 -0300

    Testando comit

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/model/Ementa.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/model/Ementa.java
@@ -5,8 +5,6 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 public class Ementa {
