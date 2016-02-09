9a8680d232afad28170b03e685014689296a13b5
Leoanrdo Rufino <leonardo.nr0@gmail.com>
2016-02-09 18:07:02 -0300

    Alterações em alguns tipos de atributos.

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/controller/HorarioAulaBean.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/controller/HorarioAulaBean.java
@@ -1,13 +1,15 @@
 package br.edu.ifba.eunapolis.gestoacademica.controller;
 
+import java.util.Calendar;
+
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "hora")
 public class HorarioAulaBean {
 
 	private String diaSemana;
-	private String horaInicio;
-	private String horaFim;
+	private Calendar horaInicio;
+	private Calendar horaFim;
 	private String mensagem;
 	
 	public void MensagemCadastro(){
@@ -21,19 +23,21 @@ public class HorarioAulaBean {
 	public String getDiaSemana() {
 		return diaSemana;
 	}
+
 	public void setDiaSemana(String diaSemana) {
 		this.diaSemana = diaSemana;
 	}
-	public String getHoraInicio() {
+	public Calendar getHoraInicio() {
 		return horaInicio;
 	}
-	public void setHoraInicio(String horaInicio) {
+	public void setHoraInicio(Calendar horaInicio) {
 		this.horaInicio = horaInicio;
 	}
-	public String getHoraFim() {
+	public Calendar getHoraFim() {
 		return horaFim;
 	}
-	public void setHoraFim(String horaFim) {
+
+	public void setHoraFim(Calendar horaFim) {
 		this.horaFim = horaFim;
 	}
 
