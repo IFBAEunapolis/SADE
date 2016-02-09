0457456d42aab169346dc7a98558c65ded4b8a76
Leoanrdo Rufino <leonardo.nr0@gmail.com>
2016-02-09 17:41:10 -0300

    Criação de Classe para inicializar EntityManager

---
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/util/JpaUtil.java
@@ -0,0 +1,20 @@
+package br.edu.ifba.eunapolis.gestoacademica.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+
+	private static EntityManagerFactory factory;
+
+	static {
+		factory = Persistence.createEntityManagerFactory("br.edu.ifba.eunapolis_SADE_war_0.0.1-SNAPSHOTPU");
+
+	}
+
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+
+}
