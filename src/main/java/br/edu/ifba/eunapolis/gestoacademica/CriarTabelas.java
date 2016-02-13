fea306ca30c6daa36fc4965bc4a4a126b493b287
Frannciel <frannciel@gmail.com>
2016-02-13 03:30:24 -0300

    verificando

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/CriarTabelas.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/CriarTabelas.java
@@ -2,12 +2,18 @@ package br.edu.ifba.eunapolis.gestoacademica;
 
 import javax.persistence.Persistence;
 
+/**
+ * Classe que cria as tabelas no banco de dados
+ *
+ * @author Leonardo
+ * @version 1.0
+ */
 public class CriarTabelas {
 
-	public static void main(String[] args) {
-		
-		Persistence.createEntityManagerFactory("br.edu.ifba.eunapolis_SADE_war_0.0.1-SNAPSHOTPU");
+    public static void main(String[] args) {
 
-	}
+        Persistence.createEntityManagerFactory("br.edu.ifba.eunapolis_SADE_war_0.0.1-SNAPSHOTPU");
+
+    }
 
 }
