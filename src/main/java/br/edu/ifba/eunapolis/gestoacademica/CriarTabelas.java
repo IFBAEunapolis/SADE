cef4e4a8fdfb86bb0ed96c69a23c54b30a225f2b
Luana <almeidah.luana@gmail.com>
2016-02-15 01:02:34 -0300

    Anotações para javadoc

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/CriarTabelas.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/CriarTabelas.java
@@ -5,13 +5,13 @@ import javax.persistence.Persistence;
 /**
  * Classe que cria as tabelas no banco de dados
  *
- * @author Leonardo
+ * @author Luana Almeida
  * @version 1.0
  */
 public class CriarTabelas {
 
     public static void main(String[] args) {
-
+        
         Persistence.createEntityManagerFactory("br.edu.ifba.eunapolis_SADE_war_0.0.1-SNAPSHOTPU");
 
     }
