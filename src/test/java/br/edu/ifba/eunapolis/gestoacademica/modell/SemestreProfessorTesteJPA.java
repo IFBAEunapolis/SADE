e2e17ec1cc164bbac320e64b3a1987e1c210e972
Luana <almeidah.luana@gmail.com>
2016-02-27 12:13:49 -0300

    teste versao 1.1

--- src/test/java/br/edu/ifba/eunapolis/gestoacademica/modell/SemestreProfessorTesteJPA.java
+++ src/test/java/br/edu/ifba/eunapolis/gestoacademica/modell/SemestreProfessorTesteJPA.java
@@ -1,79 +1,106 @@
 package br.edu.ifba.eunapolis.gestoacademica.modell;
-
 import br.edu.ifba.eunapolis.gestoacademica.dao.JpaUtil;
+import java.util.Calendar;
+
 import javax.persistence.EntityManager;
 
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import br.edu.ifba.eunapolis.gestoacademica.model.Professor;
 import br.edu.ifba.eunapolis.gestoacademica.model.Semestre;
 import br.edu.ifba.eunapolis.gestoacademica.model.SemestreProfessor;
-/**
- * Classe responsavel pelos testes da tabela SemestreProfessor
- * 
- * @author Luana
- * @version 1.0
- * */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class SemestreProfessorTesteJPA {
 
-      /**
-       * Salva um objeto do tipo SemestreProfessor no banco.
-       * 
-       */
-	
+	private static Integer idSemestreProfessorSalvo;
+	private static Integer idProfessorSalvo;
+	private static Integer idSemestreSalvo;
+
 	@Test
-	public void testSalvar() throws Exception {
-		
-		SemestreProfessor semestreProfessorTeste = new SemestreProfessor();
+	public void testAdicionar() throws Exception {
+
+		Professor professorTeste = new Professor("ProfessorTeste");
 		
 		EntityManager entityManager = JpaUtil.getEntityManager();
 		entityManager.getTransaction().begin();
+
+		entityManager.persist(professorTeste);
 		
-		Professor professor = entityManager.find(Professor.class, 2);
-		Semestre semestre = entityManager.find(Semestre.class, 2);
+		entityManager.close();
 		
+		idProfessorSalvo = professorTeste.getId();
+   
+		Semestre semestreTeste = new Semestre();
+		Calendar c  = Calendar.getInstance();
+		Calendar f = Calendar.getInstance();
+		c.set(02, 02, 2016);
+		f.set(02, 06, 2016);
+		
+		semestreTeste.setId(null);
+		semestreTeste.setAno(2016);
+		semestreTeste.setInicio(c);
+		semestreTeste.setFim(f);
+		semestreTeste.setAtivo(true);
+		semestreTeste.setPeriodoLetivo(4);
+		
+		
+		EntityManager entityManager2 = JpaUtil.getEntityManager();
+		entityManager2.getTransaction().begin();
+		
+		entityManager2.persist(semestreTeste);
+
+		entityManager2.getTransaction().commit();
+
+		entityManager2.close();
+
+		idSemestreSalvo = semestreTeste.getId();
+		
+		SemestreProfessor semestreProfessorTeste = new SemestreProfessor();
+      
+		EntityManager entityManager3 = JpaUtil.getEntityManager();
+		entityManager3.getTransaction().begin();
+
+		Semestre semestre = entityManager3.find(Semestre.class, idSemestreSalvo);
+
+		Professor professor = entityManager3.find(Professor.class, idProfessorSalvo);
+
 		semestreProfessorTeste.setProfessor(professor);
 		semestreProfessorTeste.setSemestre(semestre);
 		semestreProfessorTeste.setMaximoHorasProfessor(120);
 		semestreProfessorTeste.setTurmas(null);
-		
-		entityManager.persist(semestreProfessorTeste);
-		entityManager.getTransaction().commit();
+
+		entityManager3.persist(semestreProfessorTeste);
+		entityManager3.getTransaction().commit();
+		entityManager3.close();
+
+		idSemestreProfessorSalvo = semestreProfessorTeste.getId();
+	}
+	
+
+	@Test
+	public void testBuscar() throws Exception {
+
+		EntityManager entityManager = JpaUtil.getEntityManager();
+
+		entityManager.find(SemestreProfessor.class, idSemestreProfessorSalvo);
+
 		entityManager.close();
 
 	}
 	
-	/**
-       * Busca um objeto do tipo SemestreProfessor no banco de Dados
-       * 
-       */
-
-	@Test
-	public void testbuscar() throws Exception {
-
-		EntityManager entityManager = JpaUtil.getEntityManager();
-
-		entityManager.find(SemestreProfessor.class, 4);
-
-		entityManager.close();
-
-	}
-
-         /**
-       * Atualiza um objeto do tipo SemestreProfessor no banco de Dados
-       * 
-       */
 	@Test
 	public void testAtualizar() throws Exception {
 
 		EntityManager entityManager = JpaUtil.getEntityManager();
 		entityManager.getTransaction().begin();
 
-		SemestreProfessor semestreProfessorTeste
-		= entityManager.find(SemestreProfessor.class, 2);
-      
-		Semestre semestre = entityManager.find(Semestre.class,4);
-		
+		SemestreProfessor semestreProfessorTeste = entityManager.find(SemestreProfessor.class,
+				idSemestreProfessorSalvo);
+
+		Semestre semestre = entityManager.find(Semestre.class, idSemestreSalvo);
+
 		semestreProfessorTeste.setSemestre(semestre);
 		semestreProfessorTeste.setMaximoHorasProfessor(150);
 		semestreProfessorTeste.setTurmas(null);
@@ -83,24 +110,21 @@ public class SemestreProfessorTesteJPA {
 		entityManager.close();
 
 	}
-       /**
-       * Deleta  um objeto do tipo semestre no banco de Dados
-       * 
-       */
-
-	
+ 
 	@Test
-	public void testdelete() throws Exception {
+	public void testDelete() throws Exception {
 
 		EntityManager entityManager = JpaUtil.getEntityManager();
 		entityManager.getTransaction().begin();
 
-		SemestreProfessor semestreProfessorTeste = entityManager.find(SemestreProfessor.class, 6);
+		SemestreProfessor semestreProfessorTeste = entityManager.find(SemestreProfessor.class,
+				idSemestreProfessorSalvo);
 		entityManager.remove(semestreProfessorTeste);
 
 		entityManager.getTransaction().commit();
 		entityManager.close();
 
 	}
- 
+	
+
 }
