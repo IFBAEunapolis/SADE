26ba2778bfce7b80a24a34a2a70e7ab4c5d2c82e
Jonathas John <Jonathas John@Intersectorial>
2016-03-01 20:48:40 -0300

    Alteração de informações

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/model/Semestre.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/model/Semestre.java
@@ -10,106 +10,106 @@ import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 /**
-*
-* @author Luana Almeida
-* @version 1.0
-*/
+ *
+ * @author Luana & Jonathas "John"
+ * @version 1.1.0
+ * @since 01/03/2016
+ */
 @Entity
 @Table(name = "semestre")
 public class Semestre implements Serializable {
-	@Id
-	@GeneratedValue
-	private Integer id;
-	@Column(length = 4, nullable = false)
-	private Integer ano;
-	@Column(name = "periodo_letivo", length = 1, nullable = false)
-	private Integer periodoLetivo;
-	@Temporal(TemporalType.TIMESTAMP)
-	private Calendar inicio;
-	@Temporal(TemporalType.TIMESTAMP)
-	private Calendar fim;
-	@Column(nullable = false)
-	private Boolean ativo;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(length = 4, nullable = false)
+    private Integer ano;
+    @Column(name = "periodo_letivo", length = 1, nullable = false)
+    private Integer periodoLetivo;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar inicio;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar fim;
+    @Column(nullable = false)
+    private Boolean ativo;
+    
+    public Semestre() {
+    }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    public Integer getAno() {
+        return ano;
+    }
+    
+    public void setAno(Integer ano) {
+        this.ano = ano;
+    }
+    
+    public Integer getPeriodoLetivo() {
+        return periodoLetivo;
+    }
+    
+    public void setPeriodoLetivo(Integer periodoLetivo) {
+        this.periodoLetivo = periodoLetivo;
+    }
+    
+    public Calendar getInicio() {
+        return inicio;
+    }
+    
+    public void setInicio(Calendar inicio) {
+        this.inicio = inicio;
+    }
+    
+    public Calendar getFim() {
+        return fim;
+    }
+    
+    public void setFim(Calendar fim) {
+        this.fim = fim;
+    }
+    
+    public Boolean getAtivo() {
+        return ativo;
+    }
+    
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
 
-	public Semestre() {
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-	
-	public Integer getPeriodoLetivo() {
-		return periodoLetivo;
-	}
-
-	public void setPeriodoLetivo(Integer periodoLetivo) {
-		this.periodoLetivo = periodoLetivo;
-	}
-
-	
-	public Calendar getInicio() {
-		return inicio;
-	}
-
-	public void setInicio(Calendar inicio) {
-		this.inicio = inicio;
-	}
-
-	
-	public Calendar getFim() {
-		return fim;
-	}
-
-	public void setFim(Calendar fim) {
-		this.fim = fim;
-	}
-
-	
-	public Boolean getAtivo() {
-		return ativo;
-	}
-
-	public void setAtivo(Boolean ativo) {
-		this.ativo = ativo;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Semestre other = (Semestre) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Semestre other = (Semestre) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
