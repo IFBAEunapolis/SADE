634c49176476d0876c668d965f142ae913d5063a
Luana <almeidah.luana@gmail.com>
2016-03-09 21:47:13 -0300

    ProfessorBean CRUD

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/controller/ProfessorBean.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/controller/ProfessorBean.java
@@ -1,29 +1,84 @@
 package br.edu.ifba.eunapolis.gestoacademica.controller;
 
-import static java.lang.System.out;
-
+import br.edu.ifba.eunapolis.gestoacademica.dao.JpaUtil;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
 
+import br.edu.ifba.eunapolis.gestoacademica.model.Professor;
 
 @ManagedBean(name = "professor")
 /**
  *
- * @author Luana
+ * @author Luana Almeida
  */
+
 public class ProfessorBean {
 
 	private String idProfessor;
 	private String nomeProfessor;
 	private String semestreprofessor;
 
-	public void cadastrar() {
+	private EntityManager em = JpaUtil.getEntityManager();
+	private EntityTransaction trans = em.getTransaction();
 
-		out.println("<br/>");
-		out.println("Cadastrado com sucesso!");
-
+	public void cadastrar(Professor professor) {
+		try {
+			trans.begin();
+			em.persist(professor);
+			em.getTransaction();
+			trans.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
 	}
 
-	
+	public List<Professor> listarProfessores() {
+
+		TypedQuery<Professor> q = em.createQuery("from Professor", Professor.class);
+		List<Professor> listProfessores = q.getResultList();
+
+		return listProfessores;
+	}
+
+	public void buscarProfessor(Integer idProfessor) {
+
+		try {
+			trans.begin();
+			em.find(Professor.class, idProfessor);
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+	}
+
+	public void atualizar(Professor professor) {
+		try {
+			trans.begin();
+			em.merge(professor);
+			em.getTransaction();
+			trans.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+	}
+
+	public void Deletar(Professor professor) {
+		try {
+			trans.begin();
+			professor = em.find(Professor.class, professor.getId());
+			em.remove(professor);
+			trans.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+	}
+
 	public void setIdProfessor(String idProfessor) {
 		this.idProfessor = idProfessor;
 	}
