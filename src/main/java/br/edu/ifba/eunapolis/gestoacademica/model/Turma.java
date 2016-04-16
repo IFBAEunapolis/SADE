e61e34d568f9d2d67fe98545e67979dae4f7bbb3
Leoanrdo Rufino <leonardo.nr0@gmail.com>
2016-04-16 13:15:53 -0300

    Paginas CRUD Turma

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/model/Turma.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/model/Turma.java
@@ -1,7 +1,13 @@
 package br.edu.ifba.eunapolis.gestoacademica.model;
 
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 /**
  * @author Franciel
