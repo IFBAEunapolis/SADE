61b9ef6826d63371d6482374bac9a2ffa2d564c4
Luana Almeida <almeidah.luana@gmail.com>
2016-04-17 16:18:46 -0300

    EspaçoFisico Controller

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/presentation/EspacoFisicoController.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/presentation/EspacoFisicoController.java
@@ -1,165 +1,30 @@
 package br.edu.ifba.eunapolis.gestoacademica.presentation;
 
+
 import br.edu.ifba.eunapolis.gestoacademica.model.EspacoFisico;
-import br.edu.ifba.eunapolis.gestoacademica.presentation.util.JsfUtil;
-import br.edu.ifba.eunapolis.gestoacademica.presentation.util.JsfUtil.PersistAction;
+import br.edu.ifba.eunapolis.gestoacademica.session.AbstractFacade;
 import br.edu.ifba.eunapolis.gestoacademica.session.EspacoFisicoFacade;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ejb.EJBException;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
+import javax.faces.view.ViewScoped;
 
 @Named("espacoFisicoController")
-@SessionScoped
-public class EspacoFisicoController implements Serializable {
+@ViewScoped
+public class EspacoFisicoController extends AbstractController<EspacoFisico> {
 
     @EJB
-    private br.edu.ifba.eunapolis.gestoacademica.session.EspacoFisicoFacade ejbFacade;
-    private List<EspacoFisico> items = null;
-    private EspacoFisico selected;
-
-    public EspacoFisicoController() {
+    private EspacoFisicoFacade ejbFacade;
+    
+    public  EspacoFisicoController() {
+        super(EspacoFisico.class);
     }
 
-    public EspacoFisico getSelected() {
-        return selected;
+    @Override
+    protected AbstractFacade<EspacoFisico> getFacade() {
+           return ejbFacade;
     }
-
-    public void setSelected(EspacoFisico selected) {
-        this.selected = selected;
-    }
-
-    protected void setEmbeddableKeys() {
-    }
-
-    protected void initializeEmbeddableKey() {
-    }
-
-    private EspacoFisicoFacade getFacade() {
-        return ejbFacade;
-    }
-
-    public EspacoFisico prepareCreate() {
-        selected = new EspacoFisico();
-        initializeEmbeddableKey();
-        return selected;
-    }
-
-    public void create() {
-        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("EspacoFisicoCreated"));
-        if (!JsfUtil.isValidationFailed()) {
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
-    }
-
-    public void update() {
-        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("EspacoFisicoUpdated"));
-    }
-
-    public void destroy() {
-        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("EspacoFisicoDeleted"));
-        if (!JsfUtil.isValidationFailed()) {
-            selected = null; // Remove selection
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
-    }
-
-    public List<EspacoFisico> getItems() {
-        if (items == null) {
-            items = getFacade().findAll();
-        }
-        return items;
-    }
-
-    private void persist(PersistAction persistAction, String successMessage) {
-        if (selected != null) {
-            setEmbeddableKeys();
-            try {
-                if (persistAction != PersistAction.DELETE) {
-                    getFacade().edit(selected);
-                } else {
-                    getFacade().remove(selected);
-                }
-                JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
-                String msg = "";
-                Throwable cause = ex.getCause();
-                if (cause != null) {
-                    msg = cause.getLocalizedMessage();
-                }
-                if (msg.length() > 0) {
-                    JsfUtil.addErrorMessage(msg);
-                } else {
-                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            }
-        }
-    }
-
-    public EspacoFisico getEspacoFisico(java.lang.Long id) {
-        return getFacade().find(id);
-    }
-
-    public List<EspacoFisico> getItemsAvailableSelectMany() {
-        return getFacade().findAll();
-    }
-
-    public List<EspacoFisico> getItemsAvailableSelectOne() {
-        return getFacade().findAll();
-    }
-
-    @FacesConverter(forClass = EspacoFisico.class)
-    public static class EspacoFisicoControllerConverter implements Converter {
-
-        @Override
-        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-            if (value == null || value.length() == 0) {
-                return null;
-            }
-            EspacoFisicoController controller = (EspacoFisicoController) facesContext.getApplication().getELResolver().
-                    getValue(facesContext.getELContext(), null, "espacoFisicoController");
-            return controller.getEspacoFisico(getKey(value));
-        }
-
-        java.lang.Long getKey(String value) {
-            java.lang.Long key;
-            key = Long.valueOf(value);
-            return key;
-        }
-
-        String getStringKey(java.lang.Long value) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(value);
-            return sb.toString();
-        }
-
-        @Override
-        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-            if (object == null) {
-                return null;
-            }
-            if (object instanceof EspacoFisico) {
-                EspacoFisico o = (EspacoFisico) object;
-                return getStringKey(o.getId());
-            } else {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), EspacoFisico.class.getName()});
-                return null;
-            }
-        }
-
-    }
-
+    
+   
+    
 }
