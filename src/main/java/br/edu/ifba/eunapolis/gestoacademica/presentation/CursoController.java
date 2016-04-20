856da3000287d9b3cd09657aad5e584e563e51e6
Jonathas John <jonathas.john@outlook.com>
2016-04-20 02:36:47 -0300

    Closed #61 Closed #67
    CRUD + Teste

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/presentation/CursoController.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/presentation/CursoController.java
@@ -1,165 +1,31 @@
 package br.edu.ifba.eunapolis.gestoacademica.presentation;
 
 import br.edu.ifba.eunapolis.gestoacademica.model.Curso;
-import br.edu.ifba.eunapolis.gestoacademica.presentation.util.JsfUtil;
-import br.edu.ifba.eunapolis.gestoacademica.presentation.util.JsfUtil.PersistAction;
 import br.edu.ifba.eunapolis.gestoacademica.session.CursoFacade;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ejb.EJBException;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
 
+/**
+ *
+ * @author Jonathas 'John'
+ * @version 1.1.0
+ * @since 20/04/2016
+ */
 @Named("cursoController")
-@SessionScoped
-public class CursoController implements Serializable {
+@ViewScoped
+public class CursoController extends AbstractController<Curso> {
 
     @EJB
-    private br.edu.ifba.eunapolis.gestoacademica.session.CursoFacade ejbFacade;
-    private List<Curso> items = null;
-    private Curso selected;
+    private CursoFacade ejbFacade;
 
     public CursoController() {
+        super(Curso.class);
     }
 
-    public Curso getSelected() {
-        return selected;
-    }
-
-    public void setSelected(Curso selected) {
-        this.selected = selected;
-    }
-
-    protected void setEmbeddableKeys() {
-    }
-
-    protected void initializeEmbeddableKey() {
-    }
-
-    private CursoFacade getFacade() {
+    @Override
+    protected CursoFacade getFacade() {
         return ejbFacade;
     }
-
-    public Curso prepareCreate() {
-        selected = new Curso();
-        initializeEmbeddableKey();
-        return selected;
-    }
-
-    public void create() {
-        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("CursoCreated"));
-        if (!JsfUtil.isValidationFailed()) {
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
-    }
-
-    public void update() {
-        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("CursoUpdated"));
-    }
-
-    public void destroy() {
-        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("CursoDeleted"));
-        if (!JsfUtil.isValidationFailed()) {
-            selected = null; // Remove selection
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
-    }
-
-    public List<Curso> getItems() {
-        if (items == null) {
-            items = getFacade().findAll();
-        }
-        return items;
-    }
-
-    private void persist(PersistAction persistAction, String successMessage) {
-        if (selected != null) {
-            setEmbeddableKeys();
-            try {
-                if (persistAction != PersistAction.DELETE) {
-                    getFacade().edit(selected);
-                } else {
-                    getFacade().remove(selected);
-                }
-                JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
-                String msg = "";
-                Throwable cause = ex.getCause();
-                if (cause != null) {
-                    msg = cause.getLocalizedMessage();
-                }
-                if (msg.length() > 0) {
-                    JsfUtil.addErrorMessage(msg);
-                } else {
-                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            }
-        }
-    }
-
-    public Curso getCurso(java.lang.Integer id) {
-        return getFacade().find(id);
-    }
-
-    public List<Curso> getItemsAvailableSelectMany() {
-        return getFacade().findAll();
-    }
-
-    public List<Curso> getItemsAvailableSelectOne() {
-        return getFacade().findAll();
-    }
-
-    @FacesConverter(forClass = Curso.class)
-    public static class CursoControllerConverter implements Converter {
-
-        @Override
-        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-            if (value == null || value.length() == 0) {
-                return null;
-            }
-            CursoController controller = (CursoController) facesContext.getApplication().getELResolver().
-                    getValue(facesContext.getELContext(), null, "cursoController");
-            return controller.getCurso(getKey(value));
-        }
-
-        java.lang.Integer getKey(String value) {
-            java.lang.Integer key;
-            key = Integer.valueOf(value);
-            return key;
-        }
-
-        String getStringKey(java.lang.Integer value) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(value);
-            return sb.toString();
-        }
-
-        @Override
-        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-            if (object == null) {
-                return null;
-            }
-            if (object instanceof Curso) {
-                Curso o = (Curso) object;
-                return getStringKey(o.getId());
-            } else {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Curso.class.getName()});
-                return null;
-            }
-        }
-
-    }
-
 }
