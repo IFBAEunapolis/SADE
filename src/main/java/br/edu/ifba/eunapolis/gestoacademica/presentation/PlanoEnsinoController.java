d4d0bacf9a75f3964fa01187280c06ef9cb81142
Luana Almeida <almeidah.luana@gmail.com>
2016-04-22 02:01:04 -0300

    PlanoEnsinoController

--- src/main/java/br/edu/ifba/eunapolis/gestoacademica/presentation/PlanoEnsinoController.java
+++ src/main/java/br/edu/ifba/eunapolis/gestoacademica/presentation/PlanoEnsinoController.java
@@ -1,165 +1,30 @@
 package br.edu.ifba.eunapolis.gestoacademica.presentation;
 
 import br.edu.ifba.eunapolis.gestoacademica.model.PlanoEnsino;
-import br.edu.ifba.eunapolis.gestoacademica.presentation.util.JsfUtil;
-import br.edu.ifba.eunapolis.gestoacademica.presentation.util.JsfUtil.PersistAction;
+import br.edu.ifba.eunapolis.gestoacademica.session.AbstractFacade;
 import br.edu.ifba.eunapolis.gestoacademica.session.PlanoEnsinoFacade;
 
-import java.io.Serializable;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.ejb.EJB;
-import javax.ejb.EJBException;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.convert.Converter;
-import javax.faces.convert.FacesConverter;
 
 @Named("planoEnsinoController")
-@SessionScoped
-public class PlanoEnsinoController implements Serializable {
+@ViewScoped
+public class PlanoEnsinoController extends AbstractController<PlanoEnsino> {
 
     @EJB
-    private br.edu.ifba.eunapolis.gestoacademica.session.PlanoEnsinoFacade ejbFacade;
-    private List<PlanoEnsino> items = null;
-    private PlanoEnsino selected;
+    private PlanoEnsinoFacade ejbFacade;
+   
 
     public PlanoEnsinoController() {
+        super(PlanoEnsino.class);
     }
 
-    public PlanoEnsino getSelected() {
-        return selected;
-    }
-
-    public void setSelected(PlanoEnsino selected) {
-        this.selected = selected;
-    }
-
-    protected void setEmbeddableKeys() {
-    }
-
-    protected void initializeEmbeddableKey() {
-    }
-
-    private PlanoEnsinoFacade getFacade() {
+    @Override
+    protected AbstractFacade<PlanoEnsino> getFacade() {
         return ejbFacade;
     }
 
-    public PlanoEnsino prepareCreate() {
-        selected = new PlanoEnsino();
-        initializeEmbeddableKey();
-        return selected;
-    }
-
-    public void create() {
-        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("PlanoEnsinoCreated"));
-        if (!JsfUtil.isValidationFailed()) {
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
-    }
-
-    public void update() {
-        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("PlanoEnsinoUpdated"));
-    }
-
-    public void destroy() {
-        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("PlanoEnsinoDeleted"));
-        if (!JsfUtil.isValidationFailed()) {
-            selected = null; // Remove selection
-            items = null;    // Invalidate list of items to trigger re-query.
-        }
-    }
-
-    public List<PlanoEnsino> getItems() {
-        if (items == null) {
-            items = getFacade().findAll();
-        }
-        return items;
-    }
-
-    private void persist(PersistAction persistAction, String successMessage) {
-        if (selected != null) {
-            setEmbeddableKeys();
-            try {
-                if (persistAction != PersistAction.DELETE) {
-                    getFacade().edit(selected);
-                } else {
-                    getFacade().remove(selected);
-                }
-                JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
-                String msg = "";
-                Throwable cause = ex.getCause();
-                if (cause != null) {
-                    msg = cause.getLocalizedMessage();
-                }
-                if (msg.length() > 0) {
-                    JsfUtil.addErrorMessage(msg);
-                } else {
-                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            }
-        }
-    }
-
-    public PlanoEnsino getPlanoEnsino(java.lang.Integer id) {
-        return getFacade().find(id);
-    }
-
-    public List<PlanoEnsino> getItemsAvailableSelectMany() {
-        return getFacade().findAll();
-    }
-
-    public List<PlanoEnsino> getItemsAvailableSelectOne() {
-        return getFacade().findAll();
-    }
-
-    @FacesConverter(forClass = PlanoEnsino.class)
-    public static class PlanoEnsinoControllerConverter implements Converter {
-
-        @Override
-        public Object getAsObject(FacesContext facesContext, UIComponent component, String value) {
-            if (value == null || value.length() == 0) {
-                return null;
-            }
-            PlanoEnsinoController controller = (PlanoEnsinoController) facesContext.getApplication().getELResolver().
-                    getValue(facesContext.getELContext(), null, "planoEnsinoController");
-            return controller.getPlanoEnsino(getKey(value));
-        }
-
-        java.lang.Integer getKey(String value) {
-            java.lang.Integer key;
-            key = Integer.valueOf(value);
-            return key;
-        }
-
-        String getStringKey(java.lang.Integer value) {
-            StringBuilder sb = new StringBuilder();
-            sb.append(value);
-            return sb.toString();
-        }
-
-        @Override
-        public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
-            if (object == null) {
-                return null;
-            }
-            if (object instanceof PlanoEnsino) {
-                PlanoEnsino o = (PlanoEnsino) object;
-                return getStringKey(o.getId());
-            } else {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), PlanoEnsino.class.getName()});
-                return null;
-            }
-        }
-
-    }
-
+    
 }
